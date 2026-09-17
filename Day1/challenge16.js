a0e07ab0a546a3b1d7cd71e1b300044ccccf7015
oussamabikiche664-ai <oussamabikiche664.gmail.com>
2026-09-17 17:02:30 +0100

    modifier

---
+++ Day1/challenge16.js
@@ -0,0 +1,10 @@
+let Duree=7384 ;
+seconds= Duree % 60
+minutes= (Duree/60) % 60
+heures=(Duree/60) % 60
+
+
+console.log(" Duree : " + Duree);
+console.log("Heures : " + heures);
+console.log("minutes : " + minutes );
+console.log("Secondes :" + seconds);
--- Day1/challenges5.js
+++ Day1/challenges5.js
@@ -1,13 +1,14 @@
-let prompt= require('prompt-sync')();
-let film= prompt("allez fournit la duree du film en minutes:");
-if (film<60){
-     categorie= "Court metrage";
+let prompt = require('prompt-sync')();
+let film=prompt("Veuillez entrer la duree de film : ");
+film=Number(film)
+if (film< 60){
+    console.log("Duree :" + film + "minutes")
+     console.log("Court metrage");
 }
- if (film>60 && film<=120){
-     categorie = "Film standard ";
+else if (film>60 && film<120){
+    console.log("Duree :" + film + " minutes")
+     console.log("Film standard");
 }
-if (film > 120){
-   categorie="Film long";
-   
-} 
- console.log("Duree : "+ film + " minutes\nCategorie : " + categorie )
+else 
+     console.log("Duree :" + film + "minutes")
+console.log("Film long");
