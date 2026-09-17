54bb078716cc628d4c2ba8ad20eb2c59d27b9bcb
oussamabikiche664-ai <oussamabikiche664.gmail.com>
2026-09-17 13:40:00 +0100

    finish challenge 3 condition

---
+++ Day1/challenge3condition.js
@@ -0,0 +1,10 @@
+let prompt=require("prompt-sync")();
+note=prompt("Veuillez entrer votre note:");
+if (note>=10 && note<20){
+    console.log(" Admis");
+}
+else 
+    console.log("Non Admis");
+
+
+        
