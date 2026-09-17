1d564cdbe3c1abf50cf2597217a65bf459d8872f
oussamabikiche664-ai <oussamabikiche664.gmail.com>
2026-09-17 11:37:47 +0100

    finish challenge18

---
+++ Day1/challenge18.js
@@ -0,0 +1,9 @@
+let nom="Clavier";
+let prix=350;
+let disponible = true ;
+let stock = 12;
+
+console.log("nom "  + typeof(nom) );
+console.log("prix " + typeof(prix));
+console.log("disponible " + typeof(disponible));
+console.log("stock " + typeof(stock));
