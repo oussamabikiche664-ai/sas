00f59b1f4d678f16bb98f4a106ecf6601be2fa92
oussamabikiche664-ai <oussamabikiche664.gmail.com>
2026-09-17 15:53:07 +0100

    finish challenge 7 condition

---
+++ Day1/challenge7condition.js
@@ -0,0 +1,32 @@
+let prompt=require('prompt-sync')();
+let num=prompt("entrer un numero entre 1 et 7 : ");
+num=Number(num)
+switch (num) {
+
+    case 1 :
+        console.log("Lundi");
+        break;
+        case 2:
+            console.log("Mardi");
+            break;
+            case 3:
+                console.log("Mercredi");
+                break; 
+                case 4: 
+                console.log("Jeudi");
+                break;
+                case 5:
+                    console.log("Vendredi");
+                    break;
+                    case 6: 
+                    console.log("Samedi");
+                    break;
+                    case 7 : 
+                    console.log("Dimanche");
+                    break;
+                    default:
+                        console.log("Try again");
+
+
+
+}
