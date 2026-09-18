9ecd984bfb7bfa62022135d47d2870602c390313
oussamabikiche664-ai <oussamabikiche664.gmail.com>
2026-09-18 15:19:21 +0100

    finish challenge 12 boucles

---
+++ Day2/challenge12boucles.js
@@ -0,0 +1,8 @@
+let prompt=require('prompt-sync')();
+N=+prompt("Veuillez entrer une valeur ");
+for(let i=1 ; i<=N ; i++){
+    diviseur=N%i;
+    if (0==diviseur){
+        console.log(i);
+    }
+}
