b7dc0e3d5ed26a4920c082553ab9d316492aa185
oussamabikiche664-ai <oussamabikiche664.gmail.com>
2026-09-18 10:34:26 +0100

    finish challenge 4 boucles

---
+++ Day2/challenge4boucle.js
@@ -0,0 +1,10 @@
+let prompt=require('prompt-sync')();
+let N=prompt("entrer une nombre : ");
+N=Number(N)
+let multiple=1;
+for (i=1 ; i<=10 ; i++){
+    multiple=N*i
+    console.log(N*i )
+
+}
+console.log(multiple);
