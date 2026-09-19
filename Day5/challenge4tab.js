d937ee81375a761ebc2a0460163a2cbc7d075cee
oussamabikiche664-ai <oussamabikiche664.gmail.com>
2026-09-19 11:49:06 +0100

    finish 4 tableau

---
+++ Day5/challenge4tab.js
@@ -0,0 +1,8 @@
+let ajouterALafin=[1,2,3]
+for (let i=0 ; i<=4 ;i++){
+    if(i===3){
+        ajouterALafin[i]=4;
+    }
+}
+//console.log(ajouterALafin[3]);
+console.log("ajouterALafin" +"= " + "[" +ajouterALafin[0] +",", ajouterALafin[1] +"," , ajouterALafin[2] , "," ,ajouterALafin[3] +"]" );
