d03d463afe79eaf448c610659e0db3234bdfbe08
oussamabikiche664-ai <oussamabikiche664.gmail.com>
2026-09-19 10:54:28 +0100

    finish challenge 2 Tab

---
+++ Day5/challenge2tableau.js
@@ -0,0 +1,13 @@
+//let calculersomme=[5,10,15]
+//let somme = (calculersomme[0]+calculersomme[1]+calculersomme[2])
+//console.log("Somme : " +somme);
+
+
+
+function calculersomme(tableau){
+console.log(tableau[0]+tableau[1]+tableau[2])
+
+
+}
+let tableau=[5,10,15];
+calculersomme(tableau);
