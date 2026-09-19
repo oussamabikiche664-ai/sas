e28cc0d9ba47aeb23c94f01fed3e8a59ec09cc4f
oussamabikiche664-ai <oussamabikiche664.gmail.com>
2026-09-19 10:53:12 +0100

    finish challenge 1 Tab

---
+++ Day5/challenge1tableau.js
@@ -0,0 +1,16 @@
+//let afficherElements=[10,20,30,40]
+//console.log(afficherElements[0]);
+//console.log(afficherElements[1]);
+//console.log(afficherElements[2]);
+//console.log(afficherElements[3]);
+
+function afficherElements(tableau){
+console.log(tableau[0]);
+console.log(tableau[1]);
+console.log(tableau[2]);
+console.log(tableau[3]);
+
+}
+let tableau=[10,20,30,40];
+
+afficherElements(tableau);
