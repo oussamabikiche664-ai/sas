1236999f504ff6ef04a00aaac456bfe0225096ba
oussamabikiche664-ai <oussamabikiche664.gmail.com>
2026-09-21 11:12:25 +0000

    finish challenge 3 function

---
+++ Day 3/challenge3function.js
@@ -0,0 +1,10 @@
+function genererEmail(prenom,nom){
+
+    let Email=prenom+'.'+nom+"@entreprise.com";
+
+console.log(Email);
+}
+
+
+let email2=genererEmail("Tayeb","Souini");
+console.log(email2);
