6c72c886b3af4c21d0a2d35f9dc31bfbf0477d28
oussamabikiche664-ai <oussamabikiche664.gmail.com>
2026-09-23 11:32:11 +0000

    modification code

--- Day6/challenge3.js
+++ Day6/challenge3.js
@@ -1,8 +1,25 @@
-let product={
-    name: "TV" , price : 5000 , category : " Samsung" , quantity : 15 , available : "oui c'est disponible " 
+let prompt = require('prompt-sync')();
+let nom = prompt("Veuillez entrer nom de product : ");
+let category = prompt("Veuillez entrer la category : ");
+nom = nom.toUpperCase();
+category = category.toLowerCase();
+let product = {
+    name: "TV", price: 5000, category: "samsung", available: " "
 }
-let prixTotal= product.price * product.quantity ;
-console.log("le nom : " +product.name); 
-console.log("le prix : " +product.price + " DH");
-console.log("Categorie : " + product.category);
-console.log("le prix total du stock : " + prixTotal + " DH");
+
+if (nom == product.name && category == product.category) {
+    product.available = "oui cette produit est disponnible  "
+    console.log(product.available);
+    let quantity = + prompt("Veuillez entrer la quantity : ")
+    product[quantity] = quantity;
+    let prixTotal = product.price * quantity;
+    console.log("le nom : " + product.name);
+    console.log("le prix : " + product.price + " DH");
+    console.log("Categorie : " + product.category);
+
+    console.log("le prix total du stock : " + prixTotal + " DH");
+}
+
+else{
+    product.available = "cette produit n'est pas disponnoble ";
+console.log(product.available);}
