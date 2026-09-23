900e29e386082c91ef3c8f791478b5765491b3d7
oussamabikiche664-ai <oussamabikiche664.gmail.com>
2026-09-23 09:37:47 +0000

    finish challenge 1

---
+++ Day6/challenge1.js
@@ -0,0 +1,9 @@
+let obj={
+firstName:"Yassine" , lastName: "Amrani", Age:21 , ville : "Youssoufia"
+};
+
+console.log("Prenom : " + obj.firstName);
+console.log("Nom : " + obj.lastName);
+console.log("Age : " + obj.Age);
+console.log("ville : " + obj.ville);
+console.log(obj.firstName + " "+ obj.lastName+" a "+ obj.Age + " ans "+"et habite a " + obj.ville +".");
