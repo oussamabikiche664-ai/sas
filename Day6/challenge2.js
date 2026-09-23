e74324adb33e6350d6303fcdab5daea6beb1e3a6
oussamabikiche664-ai <oussamabikiche664.gmail.com>
2026-09-23 09:55:26 +0000

    finish challenge 2

---
+++ Day6/challenge2.js
@@ -0,0 +1,14 @@
+let user={
+    username:"Ahmed" , age: 22 , city: "Marrakech" , status:"student" 
+};
+console.log(user.username);
+console.log(user.age);
+user.age=25;
+user.city="Youssoufia";
+user.status="Employed";
+user.email="ahmed112@gmail.com";
+console.log("Name : " + user.username );
+console.log("Age : " + user.age );
+console.log("city : " + user.city );
+console.log("status : " + user.status );
+console.log("Email : " + user.email );
